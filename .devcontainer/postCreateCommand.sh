34d1edbf618a4f0894d5dea9adc116ff1c3d50ce
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-03-28 15:38:05 -0400

    It looks like you have created a new Docker feature for Ollama, which is a model serving platform. The `devcontainer.json` file defines the metadata and configuration for this feature, such as its name, version, description, options, entrypoint, installsAfter, mounts, onCreateCommand, appPort, and containerEnv.
    The `install.sh` script checks if required packages are installed and installs them if not. It then downloads and runs the Ollama installation script to set up the platform. The `oncreate.sh` file is a custom script that pulls specified models when the container is created.
    
    The `postCreateCommand.sh` script, which was previously present in the repository, has been removed. This script was used to install and update specific Ollama models after the container creation process. It appears that this functionality has been moved to the `ollama pull` commands within the `oncreate.sh` file instead.
    
    Overall, it seems that this feature is designed to create a Docker container with Ollama installed and configured to serve specific models when the container is created or updated.

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,83 +1,11 @@
 #!/bin/bash
 #
 
-if [ -f /etc/apt/sources.list.d/cuda-ubuntu2004-x86_64.list ]; then
-  sudo rm /etc/apt/sources.list.d/cuda-ubuntu2004-x86_64.list
-fi
+#if [ -f /etc/apt/sources.list.d/cuda-ubuntu2004-x86_64.list ]; then
+#  sudo rm /etc/apt/sources.list.d/cuda-ubuntu2004-x86_64.list
+#fi
 sudo apt update
 sudo apt-get -y upgrade
 sudo apt-get -y install tensorrt cudnn cudnn-cuda-12
 sudo apt -y autoremove
-
-ollama show --modelfile codellama:34b 1> /dev/null
-if ! [ $? -eq 0 ]
-then
-  ollama pull codellama:34b
-fi
-
-ollama show --modelfile codellama:34b-instruct 1> /dev/null
-if ! [ $? -eq 0 ]
-then
-  ollama pull codellama:34b-instruct
-fi
-
-ollama show --modelfile codellama:34b-instruct 1> /dev/null
-if ! [ $? -eq 0 ]
-then
-  ollama pull codellama:34b-instruct
-fi
-
-ollama show --modelfile dolphin-mixtral:8x7b 1> /dev/null
-if ! [ $? -eq 0 ]
-then
-  ollama pull dolphin-mixtral:8x7b
-fi
-
-ollama show --modelfile llama2:13b 1> /dev/null
-if ! [ $? -eq 0 ]
-then
-  ollama pull llama2:13b
-fi
-
-ollama show --modelfile llama2-uncensored:7b 1> /dev/null
-if ! [ $? -eq 0 ]
-then
-  ollama pull llama2-uncensored:7b
-fi
-
-ollama show --modelfile mistral-openorca 1> /dev/null
-if ! [ $? -eq 0 ]
-then
-  ollama pull mistral-openorca
-fi
-
-ollama show --modelfile mixtral:8x7b 1> /dev/null
-if ! [ $? -eq 0 ]
-then
-  ollama pull mixtral:8x7b
-fi
-
-ollama show --modelfile mixtral:instruct 1> /dev/null
-if ! [ $? -eq 0 ]
-then
-  ollama pull mixtral:instruct
-fi
-
-ollama show --modelfile nomic-embed-text 1> /dev/null
-if ! [ $? -eq 0 ]
-then
-  ollama pull nomic-embed-text
-fi
-
-ollama show --modelfile nous-hermes2-mixtral:8x7b 1> /dev/null
-if ! [ $? -eq 0 ]
-then
-  ollama pull nous-hermes2-mixtral:8x7b
-fi
-
-ollama show --modelfile starcoder:3b 1> /dev/null
-if ! [ $? -eq 0 ]
-then
-  ollama pull starcoder:3b
-fi
 
